39975dd479361a996953743481eeb9a5bb61be5d
unknown <josdanguerra@gmail.com>
2022-01-22 08:51:30 -0500

    funcional con validaciones

--- src/middlewares/verifySignup.js
+++ src/middlewares/verifySignup.js
@@ -1,6 +1,20 @@
 import User from "../models/User"
 import {ROLES} from "../models/Role"
 
+
+const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username})
+        if(user) return res.status(400).json({message: "The user already exists"})
+        const email = await User.findOne({email: req.body.email})
+        if(email) return res.status(400).json({message: "The email already exists"})
+
+        next()
+    } catch (error) {
+        return res.status(500).json({message: "ERROR!" + error})
+    }
+}
+
 const checkRolesExisted = (req, res, next) => {
     if(req.body.roles){
         for(let i = 0; i<req.body.roles.length; i++){
@@ -13,4 +27,4 @@ const checkRolesExisted = (req, res, next) => {
     }
 }
 
-export {checkRolesExisted}
+export {checkRolesExisted, checkDuplicateUsernameOrEmail}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,16 @@
 import {Router} from 'express'
 import * as authCtrl from '../controllers/auth.controller'
-
+import {verifySignup} from '../middlewares'
 
 const router  = Router()
+router.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    )
+})
 
-router.post('/signup',authCtrl.signUp)
-router.post('/signin',authCtrl.signIn)
+router.post('/signup', [verifySignup.checkDuplicateUsernameOrEmail, verifySignup.checkRolesExisted], authCtrl.signUp)
+router.post('/signin', authCtrl.signIn)
 
 export default router
